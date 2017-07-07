f04221a92dedad330f07790835562a5b9de419ec
Federico Brigante <github@bfred.it>
2017-07-07 19:46:08 +0800

    Add DocumentFragment alternative return (#1)

--- readme.md
+++ readme.md
@@ -27,6 +27,16 @@ linkifyIssues('Fixes #143 and avajs/ava#1023', {
 	}
 });
 //=> 'Fixes <a href="https://github.com/sindresorhus/dofle/issues/143" class="unicorn" multiple="a b" number="1" include>#143</a> and <a href="https://github.com/avajs/ava/issues/1023" class="unicorn" multiple="a b" number="1" include>avajs/ava#1023</a>'
+
+const fragment = linkifyUrls('See #143', {
+	user: 'sindresorhus',
+	repo: 'dofle',
+ 	type: 'dom',
+ 	attributes: {
+ 		class: 'unicorn',
+ 	}
+ });
+ document.body.appendChild(fragment);
 ```
 
 
@@ -68,6 +78,18 @@ Type: `string`<br>
 Default: `https://github.com`
 
 Base URL.
+
+##### type
+
+Type: `string`<br>
+Values: `string` `dom`<br>
+Default: `string`
+
+Format of the generated content.
+
+`string` will return it as a flat string like `'See <a href="https://github.com/sindresorhus/dofle/issue/143">#143</a>'`.
+
+`dom` will return it as a `DocumentFragment` ready to be appended in a DOM safely, like `DocumentFragment(TextNode('See '), HTMLAnchorElement('#143'))`. This type only works in the browser.
 
 
 ## Related
--- test.js
+++ test.js
@@ -1,5 +1,29 @@
 import test from 'ava';
+import {jsdom} from 'jsdom';
 import m from '.';
+
+const document = jsdom();
+global.document = document;
+
+// Ponyfill until this is in:
+// https://github.com/tmpvar/jsdom/issues/317
+document.createRange = () => ({
+	createContextualFragment(html) {
+		const el = document.createElement('template');
+		el.innerHTML = html;
+		return el.content;
+	}
+});
+
+// Get DOM node from HTML
+const domify = html => document.createRange().createContextualFragment(html);
+
+// Get HTML from DOM node
+const html = dom => {
+	const el = document.createElement('div');
+	el.appendChild(dom);
+	return el.innerHTML;
+};
 
 test('main', t => {
 	t.is(
@@ -43,5 +67,20 @@ test('baseUrl option', t => {
 			baseUrl: ''
 		}),
 		'Fixes <a href="/avajs/ava/issues/1023">avajs/ava#1023</a>'
+	);
+});
+
+test('DocumentFragment support', t => {
+	t.is(
+		html(m('See #1', {
+			user: '<script></script>',
+			repo: 'x',
+			baseUrl: '',
+			type: 'dom',
+			attributes: {
+				class: '<script></script>'
+			}
+		})),
+		html(domify('See <a href="/&lt;script&gt;&lt;/script&gt;/x/issues/1" class="&lt;script&gt;&lt;/script&gt;">#1</a>'))
 	);
 });
