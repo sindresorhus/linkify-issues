ac5ccbbd9489aa1326f204e55216b7e7aa34fd42
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-17 22:12:12 +0200

    Require Node.js 8, rename `repo` option to `repository`, add TypeScript definition

--- index.js
+++ index.js
@@ -6,53 +6,55 @@ const groupedIssueRegex = new RegExp(`(${issueRegex().source})`, 'g');
 
 // Get `<a>` element as string
 const linkify = (match, options) => {
-	const fullReference = match.replace(/^#/, `${options.user}/${options.repo}#`);
-	const [userRepo, issue] = fullReference.split('#');
-	const href = `${options.baseUrl}/${userRepo}/issues/${issue}`;
+	const fullReference = match.replace(/^#/, `${options.user}/${options.repository}#`);
+	const [userRepository, issue] = fullReference.split('#');
+	const href = `${options.baseUrl}/${userRepository}/issues/${issue}`;
 
 	return createHtmlElement({
 		name: 'a',
-		attributes: Object.assign({href: ''}, options.attributes, {href}),
-		value: match
+		// First `href` is needed for the `href` attribute to be the first attribute on the `a` tag
+		attributes: {href: '', ...options.attributes, href}, // eslint-disable-line no-dupe-keys
+		text: match
 	});
 };
 
 // Get DOM node from HTML
 const domify = html => document.createRange().createContextualFragment(html);
 
-const getAsString = (input, options) => {
-	return input.replace(groupedIssueRegex, match => linkify(match, options));
+const getAsString = (text, options) => {
+	return text.replace(groupedIssueRegex, match => linkify(match, options));
 };
 
-const getAsDocumentFragment = (input, options) => {
-	return input.split(groupedIssueRegex).reduce((frag, text, index) => {
+const getAsDocumentFragment = (text, options) => {
+	return text.split(groupedIssueRegex).reduce((fragment, text, index) => {
 		if (index % 2) { // URLs are always in odd positions
-			frag.appendChild(domify(linkify(text, options)));
+			fragment.append(domify(linkify(text, options)));
 		} else if (text.length > 0) {
-			frag.appendChild(document.createTextNode(text));
+			fragment.append(document.createTextNode(text));
 		}
 
-		return frag;
+		return fragment;
 	}, document.createDocumentFragment());
 };
 
-module.exports = (input, options) => {
-	options = Object.assign({
+module.exports = (text, options) => {
+	options = {
 		attributes: {},
 		baseUrl: 'https://github.com',
-		type: 'string'
-	}, options);
+		type: 'string',
+		...options
+	};
 
-	if (!(options.user && options.repo)) {
-		throw new Error('Missing required `user` and `repo` options');
+	if (!(options.user && options.repository)) {
+		throw new Error('Missing required `user` and `repository` options');
 	}
 
 	if (options.type === 'string') {
-		return getAsString(input, options);
+		return getAsString(text, options);
 	}
 
 	if (options.type === 'dom') {
-		return getAsDocumentFragment(input, options);
+		return getAsDocumentFragment(text, options);
 	}
 
 	throw new Error('The type option must be either dom or string');
