9cd14aa55d577637c8caf4812c8d4bb6e841818e
Federico Brigante <me@fregante.com>
2024-09-04 21:56:09 +0700

    Convert test to snapshots; test both exports equally (#17)

--- test.js
+++ test.js
@@ -6,79 +6,55 @@ const dom = new jsdom.JSDOM();
 globalThis.window = dom.window;
 globalThis.document = dom.window.document;
 
-// Get DOM node from HTML
-const domify = html => document.createRange().createContextualFragment(html);
-
-// Get HTML from DOM node
-const html = dom => {
+/** Get HTML from DOM node */
+function html(dom) {
 	const element = document.createElement('div');
 	element.append(dom);
 	return element.innerHTML;
-};
+}
 
-test('main', t => {
-	t.is(
-		linkifyIssuesToHtml('Fixes #143 and avajs/ava#1023', {user: 'sindresorhus', repository: 'dofle'}),
-		'Fixes <a href="https://github.com/sindresorhus/dofle/issues/143">#143</a> and <a href="https://github.com/avajs/ava/issues/1023">avajs/ava#1023</a>',
-	);
-});
+for (const [name, linkify] of Object.entries({
+	linkifyIssuesToHtml,
+	linkifyIssuesToDom: (...arguments_) => `DocumentFragment: ${html(linkifyIssuesToDom(...arguments_))}`,
+})) {
+	test(name + ': main', t => {
+		t.snapshot(
+			linkify('Fixes #143 and avajs/ava#1023', {user: 'sindresorhus', repository: 'dofle'}),
+		);
+	});
 
-test('escapes user input', t => {
-	t.is(
-		linkifyIssuesToHtml('See #1', {
-			user: '<script></script>',
-			repository: 'x',
-			attributes: {
-				class: '<script></script>',
-			},
-		}),
-		'See <a href="https://github.com/&lt;script&gt;&lt;/script&gt;/x/issues/1" class="&lt;script&gt;&lt;/script&gt;">#1</a>',
-	);
-});
+	test(name + ': escapes user input', t => {
+		t.snapshot(
+			linkify('See #1', {
+				user: '<script></script>',
+				repository: 'x',
+				attributes: {
+					class: '<script></script>',
+				},
+			}),
+		);
+	});
 
-test('attributes option', t => {
-	t.is(
-		linkifyIssuesToHtml('See avajs/ava#1023', {
-			user: 'x',
-			repository: 'x',
-			attributes: {
-				class: 'unicorn',
-				target: '_blank',
-			},
-		}),
-		'See <a href="https://github.com/avajs/ava/issues/1023" class="unicorn" target="_blank">avajs/ava#1023</a>',
-	);
-});
+	test(name + ': attributes option', t => {
+		t.snapshot(
+			linkify('See avajs/ava#1023', {
+				user: 'x',
+				repository: 'x',
+				attributes: {
+					class: 'unicorn',
+					target: '_blank',
+				},
+			}),
+		);
+	});
 
-test('baseUrl option', t => {
-	t.is(
-		linkifyIssuesToHtml('Fixes avajs/ava#1023', {
-			user: 'x',
-			repository: 'x',
-			baseUrl: '',
-		}),
-		'Fixes <a href="/avajs/ava/issues/1023">avajs/ava#1023</a>',
-	);
-});
-
-test('DocumentFragment support', t => {
-	t.is(
-		html(linkifyIssuesToDom('See #1', {
-			user: '<script></script>',
-			repository: 'x',
-			baseUrl: '',
-			attributes: {
-				class: '<script></script>',
-			},
-		})),
-		html(domify('See <a href="/&lt;script&gt;&lt;/script&gt;/x/issues/1" class="&lt;script&gt;&lt;/script&gt;">#1</a>')),
-	);
-	t.is(
-		html(linkifyIssuesToDom('See #1 for more info', {user: 'sindresorhus', repository: 'linkify-issues', type: 'dom'})),
-		html(domify('See <a href="https://github.com/sindresorhus/linkify-issues/issues/1">#1</a> for more info')),
-	);
-	t.is(
-		html(linkifyIssuesToDom('#1 fixes refined-github/refined-github#6930', {user: 'sindresorhus', repository: 'linkify-issues', type: 'dom'})),
-		html(domify('<a href="https://github.com/sindresorhus/linkify-issues/issues/1">#1</a> fixes <a href="https://github.com/refined-github/refined-github/issues/6930">refined-github/refined-github#6930</a>')),
-	);
-});
+	test(name + ': baseUrl option', t => {
+		t.snapshot(
+			linkify('Fixes avajs/ava#1023', {
+				user: 'x',
+				repository: 'x',
+				baseUrl: '',
+			}),
+		);
+	});
+}
