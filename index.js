6448d99ac8639d146f6a45284fb3204f95283154
Dave Nicolson <david.nicolson@gmail.com>
2025-08-03 22:53:31 +0200

    Update index.js
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import createHtmlElement from 'create-html-element';
 Generates a new regex that can be used with `String#split`. The resulting array will include the whole match, rather than dropping it.
 */
 function prepareRegexForSplit(regex) {
-	return new RegExp(`(${regex.source})`, 'gi');
+	return new RegExp(`(${regex.source})`, regex.flags);
 }
 
 /**
